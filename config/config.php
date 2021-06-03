f92a812a3aaf619e48f857436d5ce149a59a61e1
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-06-03 21:09:25 +0200

    add local BDD

--- config/config.php
+++ config/config.php
@@ -1,4 +1,15 @@
 <?php
+// BDD DISTANTE FREESQL
+
+
 $DB_DSN = 'mysql:host=sql11.freemysqlhosting.net;dbname=sql11416827';
 $DB_USER = 'sql11416827';
 $DB_PASSWORD = 'q2EMMmzdBZ';
+
+
+
+// BDD LOCALE
+
+// $DB_DSN = 'mysql:host=localhost;dbname=cave';
+// $DB_USER = 'root';
+// $DB_PASSWORD = '';
