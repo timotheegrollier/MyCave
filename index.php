326c96cf54aebec1f268368bc9a5baa79f6db2af
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-06-11 12:43:35 +0200

    Hide

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<!-- Router si je prend la valeur de $_GET['action'] et j' appelle la méthode de mon controller correspondant
+<!-- Router je prend la valeur de $_GET['action'] et j' appelle la méthode de mon controller correspondant
 Je fais un require de mon controller afin de pouvoir éxécuté les méthodes stocké dedans -->
 <?php
 require('controller/controller.php');
 // je  vérifie si j'ai une clef action dans mon $_GET['action']
 
 if (isset($_GET['action'])) {
-    // je  vérifie si ma clef action dans mon $_GET est == à list-users
     if ($_GET['action'] == "home") {
         index();
     } elseif ($_GET['action'] == "cave") {
