b7d6b30204c7574261d3968c7b57d815d5040498
Timo <timotheegrollier@gmail.com>
2022-01-20 17:25:44 +0100

    Dynamic footer date

--- view/parts/footer.php
+++ view/parts/footer.php
@@ -1,13 +1,11 @@
 <footer class="text-center text-white fixed-bottom d-flex w-100 flex-nowrap" id="footerCop">
 
     <div class="text-center text-white p-3 d-flex justify-content-start flex-fill align-items-center">
-        © 2021:
+        © <?= date("Y") ?> :
         <a class="text-white" href="https://timotheegrollier.github.io/Portfolio/">Timothée Grollier</a>
     </div>
     <div class="footer-icons d-flex justify-content-end">
-        <a class="btn btn-link btn-floating btn-lg text-light" href="#!" role="button" data-mdb-ripple-color="dark"><i
-                class="fab fa-linkedin"></i></a>
-        <a class="btn btn-link btn-floating btn-lg text-light " href="https://github.com/timotheegrollier" role="button"
-            data-mdb-ripple-color="dark"><i class="fab fa-github"></i></a>
+        <a class="btn btn-link btn-floating btn-lg text-light" href="#!" role="button" data-mdb-ripple-color="dark"><i class="fab fa-linkedin"></i></a>
+        <a class="btn btn-link btn-floating btn-lg text-light " href="https://github.com/timotheegrollier" role="button" data-mdb-ripple-color="dark"><i class="fab fa-github"></i></a>
     </div>
 </footer>
